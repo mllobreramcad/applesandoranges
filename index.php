b524e8d21357e234377af401e7155fa421e2fed8
alikatmn <allisonklement@allisons-iMac.local>
2014-02-06 23:18:26 -0600

    i don't even know anymore, I am confusing myself

--- index.php
+++ index.php
@@ -2,25 +2,25 @@
 	<?php get_header(); ?>
 		<div class="clearfix">
 
-		<section>
+		<section class="tagline">
 	
-			<section>
-				<p>Apples and Oranges! is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
+			<p>Apples and Oranges! is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
 				<h2>Not just good. Uncomparable.</h2>
 
-			</section> 
+		</section> 
 
-		</section>
+		</div>
 
+
+
+		<div class="clearfix">
 		<article class="main">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<a href="<?php the_permalink(); ?>"><h2><?php the_title_attribute(); ?></h2></a>
 			<?php the_content(); ?>
 
-			<!--<p>Pickled labore ad, Thundercats church-key kogi selvage mustache tempor tousled lo-fi hella direct trade bespoke stumptown. High Life in locavore, pariatur American Apparel sunt fixie odio. Exercitation beard incididunt post-ironic chillwave tattooed, enim Shoreditch Blue Bottle artisan cornhole dreamcatcher aliqua craft beer photo booth. Butcher cred pug Etsy nulla, XOXO Bushwick forage farm-to-table Godard tousled fingerstache in deep v. Blog labore hella sartorial aesthetic, chia leggings authentic. Marfa adipisicing voluptate iPhone anim mlkshk biodiesel in. Truffaut authentic sapiente aliquip lomo cray, id veniam Marfa accusamus tousled artisan Terry Richardson eiusmod.</p>-->
-
-
-			</article>
+		</article>
+		</div>
 
 			<article class="col1">
 			
@@ -31,8 +31,6 @@
 		<?php endwhile; else : ?>
 		<p><?php_e('Sorry, no content'); ?></p>
 	<?php endif; ?>
-
-		</div>
 
 
 
