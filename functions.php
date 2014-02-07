492c74ec573245fab02fb975c355bdcf5054741e
alikatmn <allisonklement@allisons-iMac.local>
2014-02-06 22:52:32 -0600

    changed name to col1 to see if it would change

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php register_nav_menus(); 
 
 register_sidebar( array (
-	'before_widget' => 'div class="sidebar">',
+	'before_widget' => 'div class="col1">',
 	'after_widget' => '</div>',
 	'before_title' => '</div>',
 	));
